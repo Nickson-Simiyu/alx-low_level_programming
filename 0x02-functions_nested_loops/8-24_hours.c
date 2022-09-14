3c3499098747d9f09d1c1b328b1e602e565fa1c8
Nickson-Simiyu <simiyunickson1@gmail.com>
2022-09-14 07:27:35 -0700

    hot fix

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,36 +7,41 @@
 
 void jack_bauer(void)
 {
-	int i;
-	int j;
+	int h1;
+	int h2;
+	int m1;
+	int m2;
+	int a = 9;
 
-	for (i = 0; i < 24; i++)
+	h2 = 0;
+	while (h2 <= 2)
 	{
-		for (j = 0; j < 60; j++)
+		if (h2 == 2)
 		{
-			if (i < 10)
-			{
-				_putchar('0');
-				_putchar((i / 10) + '0');
-			}
-			else if (i >= 10)
-			{
-				_putchar((i / 10) + '0');
-				_putchar((i % 10) + '0');
-			}
-			if (j < 10)
-			{
-				_putchar(':');
-				_putchar('0');
-				_putchar((j / 10) + '0');
-			}
-			else if (j >= 10)
-			{
-				_putchar(':');
-				_putchar((j / 10) + '0');
-				_putchar((j % 10) + '0');
-				_putchar('\n');
-			}
+			/*Restrain to 23h, not 29*/
+			a = 3;
 		}
+		h1 = 0;
+		while (h1 <= a)
+		{
+			m2 = 0;
+			while (m2 <= 5)
+			{
+				m1 = 0;
+				while (m1 <= 9)
+				{
+					_putchar('0' + h2);
+					_putchar('0' + h1);
+					_putchar(':');
+					_putchar('0' + m2);
+					_putchar('0' + m1);
+					_putchar('\n');
+					m1++;
+				}
+			}
+			m2++;
+		}
+		h1++;
 	}
+	h2++;
 }
