a826d529c46c9dd936c2d0eb1f882515aba43ad6
Nickson-Simiyu <simiyunickson1@gmail.com>
2022-09-14 06:12:17 -0700

    hot fix

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -19,7 +19,7 @@ void jack_bauer(void)
 				_putchar('0');
 				_putchar(i + '0');
 			}
-			else if (i >= 10)
+			else if (i > 10)
 			{
 				_putchar((i / 10) + '0');
 				_putchar((i % 10) + '0');
@@ -30,7 +30,7 @@ void jack_bauer(void)
 				_putchar('0');
 				_putchar(j + '0');
 			}
-			else if (j >= 10)
+			else if (j > 10)
 			{
 				_putchar(':');
 				_putchar((j / 10) + '0');
