aabc92ad4d31bcb639c1c4af7de0e1f6cee7eae8
Nickson-Simiyu <simiyunickson1@gmail.com>
2022-09-14 03:09:44 -0700

    prints the last digit of a number

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+*print_last_digit - return last digit
+*
+*@n:number to check
+*
+*Return:0 or 1
+*/
+
+int print_last_digit(int n);
+{
+	if (n < 0)
+		n = -n;
+	x = n % 10;
+	_putchar(x + '0');
+	return (n % 10);
+}
