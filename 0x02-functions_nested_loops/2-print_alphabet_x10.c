c781efce8de1d1d61b4457d59ff486facaf0b99e
Nickson-Simiyu <simiyunickson1@gmail.com>
2022-09-14 01:17:55 -0700

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+*print_alphabet_x10 - prints alphabets ten times
+*
+*Return:void
+*/
+
+void print_alphabet_x10(void)
+{
+	int j;
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (j = 'a'; j <= 'z'; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+	}
+}
+
