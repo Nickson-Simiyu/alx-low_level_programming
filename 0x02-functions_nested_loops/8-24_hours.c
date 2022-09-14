173b9a6678d197c9e4ed2dec547a30fce20175e7
Nickson-Simiyu <simiyunickson1@gmail.com>
2022-09-14 05:41:18 -0700

    function that prints every minute

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,6 +8,7 @@
 void jack_bauer(void)
 {
 	int i, j;
+
 	for (i = 0; i < 24; i++)
 	{
 		for (j = 0; j < 60; j++)
@@ -28,7 +29,7 @@ void jack_bauer(void)
 				_putchar('0');
 				_putchar(j + '0');
 			}
-			else if (j > 10)
+			else if (j >= 10)
 			{
 				_putchar(':');
 				_putchar((j / 10) + '0');
