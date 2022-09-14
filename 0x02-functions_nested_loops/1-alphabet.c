fbc512a34fc712ae334afd1c1a51e81c5b6ed8c1
Nickson-Simiyu <simiyunickson1@gmail.com>
2022-09-14 00:39:33 -0700

    prints the alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* rint_alphabet - Entry point
+*
+* Return: Always 0.
+*/
+
+void print_alphabet(void)
+{
+	int j;
+
+	for (j = 'a'; j <= 'z'; j++)
+	{
+		_putchar(j);
+	}
+	_putchar('\n');
+}
