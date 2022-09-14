3cab04c43758a0602c7261e80e010f8d5f4fe5f9
Nickson-Simiyu <simiyunickson1@gmail.com>
2022-09-14 12:53:02 -0700

    hot fix

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -15,16 +15,15 @@ int main(void)
 	int i = 0;
 	unsigned long int a = 0, b = 1, next = 0;
 
-	while (i < 98)
+	while (i < 98; i++)
 	{
 		next = a + b;
 		a = b;
 		b = next;
 		printf("%lu", next);
 
-		if (i < 97)
+		if (i < 97; i++)
 			printf(", ");
-		i++;
 	}
 _putchar('\n');
 	return (0);
