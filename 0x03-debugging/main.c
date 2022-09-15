69f62ec39f883b7768c03beb825f4cf0616cbb90
Nickson-Simiyu <simiyunickson1@gmail.com>
2022-09-15 04:28:17 -0700

    hot fix

---
+++ 0x03-debugging/main.c
@@ -0,0 +1,18 @@
+#include <stdiolib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+*main- debugging example
+*
+*Return: 0
+*/
+int main(void)
+{
+	int 1;
+
+	i = 0;
+	positive_or_negative(i);
+	return (0);
+}
