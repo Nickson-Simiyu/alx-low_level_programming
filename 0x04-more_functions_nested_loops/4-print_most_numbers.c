4e8b2e1013fe0cb2006bcf286fdee76a0e78e21c
Nickson-Simiyu <simiyunickson1@gmail.com>
2022-09-15 12:45:10 -0700

    numbers and signs

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+*print_most_numbers- prints the numbers
+*/
+
+void print_most_numbers(void)
+{
+	for (x = 0; x < 10; x++)
+	{
+		_putchar(x + '0');
+	}
+	_putchar('\n');
+}
