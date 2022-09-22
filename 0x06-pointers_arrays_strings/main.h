bcd7bfd5d7cf997075bce47635f0fe14d50a4a85
Nickson-Simiyu <simiyunickson1@gmail.com>
2022-09-22 14:18:24 -0700

    test

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 #include <stdio.h>
 
 int _putchar(char);
@@ -13,7 +14,6 @@ char *cap_string(char *);
 char *leet(char *);
 char *rot13(char *);
 void print_number(int n);
-int main(void);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
 
