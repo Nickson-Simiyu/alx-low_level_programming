20610eb0cc9f44b3d5e66bcf8e563094ad7fa278
Nickson-Simiyu <simiyunickson1@gmail.com>
2022-09-29 13:53:08 -0700

    hotfix

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,22 +1,15 @@
-#!/bin/bash
 #include <stdio.h>
 
 /**
-
- *main - Entry point
-
- *My hello, printf task
-
- *Return: return 1 after program execution
-
+ * main - Entry point
+ *
+ * Description: prints "with proper grammar, but the outcome is a piece of
+ * art,"
+ *
+ * Return: Always 0 (Success)
  */
-
 int main(void)
-
 {
-
-printf(with proper grammar, but the outcome is a piece of art,n);
-
-return (0);
-
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
