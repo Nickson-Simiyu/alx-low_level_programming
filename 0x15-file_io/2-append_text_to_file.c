f21545573e1d8c09f21319ac9ee456caed10be2c
Nickson-Simiyu <simiyunickson1@gmail.com>
2022-11-03 01:34:56 -0700

    fix

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -10,30 +10,26 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-        int fd;
-        int nletters;
-        int rwr;
+	int i = 0, file;
 
-        if (!filename)
-                return (-1);
+	if (filename == NULL)
+		return (-1);
 
-        fd = open(filename, O_WRONLY | O_APPEND);
+	if (text_content == NULL)
+		text_content = "";
 
-        if (fd == -1)
-                return (-1);
 
-        if (text_content)
-        {
-                for (nletters = 0; text_content[nletters]; nletters++)
-                        ;
+	while (text_content[i] != '\0')
+	{
+		i++;
+	}
 
-                rwr = write(fd, text_content, nletters);
+	file = open(filename, O_WRONLY | O_APPEND);
 
-                if (rwr == -1)
-                        return (-1);
-        }
+	if (file == -1)
+		return (-1);
 
-        close(fd);
+	write(file, text_content, i);
 
-        return (1);
+	return (1);
 }
