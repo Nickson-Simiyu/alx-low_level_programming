d6281a3938caa67ff29dff9d2b315cb408f6ee80
Nickson-Simiyu <simiyunickson1@gmail.com>
2022-12-14 03:33:07 -0800

    task 0

---
+++ 0x18-dynamic_libraries/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+*_islower - check for lower case letter
+*
+*@c : character to check the case
+*
+*Return:0 or 1 depending on condition
+*/
+
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
+
